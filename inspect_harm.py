85ca99d78402eb5209e93d802a8a40744b5dd404
dms0313 <dms0313@gmail.com>
2025-12-05 09:34:26 -0600

    removed old scripts

--- inspect_harm.py
+++
@@ -1,7 +0,0 @@
-
-import google.generativeai as genai
-from google.generativeai.types import HarmCategory
-
-print("HarmCategory members:")
-for member in HarmCategory:
-    print(f"{member.name} = {member.value}")
